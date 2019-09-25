22fc5110420c61cc44b385e5c0b64bc6a31aef02
Argonus <argonuspiotr@gmail.com>
2019-09-25 20:29:12 +0200

    add flag disabling goth start

--- lib/goth.ex
+++ lib/goth.ex
@@ -7,6 +7,14 @@ defmodule Goth do
 
   # for now, just spin up the supervisor
   def start(_type, _args) do
+    if Application.get_env(:disable_on_start) do
+      Supervisor.start_link([], strategy: :one_for_one)
+    else
+      start_link()
+    end
+  end
+
+  defp start_link() do
     envs = Application.get_all_env(:goth)
     Goth.Supervisor.start_link(envs)
   end
